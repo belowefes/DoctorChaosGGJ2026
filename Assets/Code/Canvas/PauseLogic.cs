7f03183fb2a9bd170e4f1890b56a01ec2c89fe44
Dan Lvovich <23659644+Toru8P@users.noreply.github.com>
2026-01-31 17:16:10 +0200

    Input pause from gamepad

--- Assets/Code/Canvas/PauseLogic.cs
+++ Assets/Code/Canvas/PauseLogic.cs
@@ -1,40 +1,61 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PauseLogic : MonoBehaviour
 {
-    [SerializeField] private GameObject pausePanel;   
+    [SerializeField] private InputAction pauseInput;
+
     [SerializeField] private List<GameObject> turnOffAfterPause;
     [SerializeField] private List<GameObject> turnOnAfterPause;
 
     private bool _paused = false;
-    
+
     public void TogglePause()
     {
         _paused = !_paused;
         foreach (var target in turnOffAfterPause)
         {
             target.SetActive(!_paused);
-        } 
-        
+        }
+
         foreach (var target in turnOnAfterPause)
         {
             target.SetActive(_paused);
-        } 
-        
+        }
+
         if (_paused)
         {
-            Time.timeScale = 0f;   
+            Time.timeScale = 0f;
         }
         else
         {
             Time.timeScale = 1f;
         }
     }
-    
-    
+
+
     void Start()
     {
         TogglePause();
+    }
+
+    void OnEnable()
+    {
+        pauseInput.Enable();
+    }
+
+    void OnDisable()
+    {
+        pauseInput.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        if (pauseInput.IsPressed())
+        {
+            TogglePause();
+        }
     }
 }

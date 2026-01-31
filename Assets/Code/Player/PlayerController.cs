dde715beaef5d9c8bd3862c7bb2bdf3a82112f9a
IvanLibkov <libkov.ivan@gmail.com>
2026-01-31 13:30:54 +0200

    Music

--- Assets/Code/Player/PlayerController.cs
+++ Assets/Code/Player/PlayerController.cs
@@ -2,20 +2,19 @@ using System;
 using Code.Inventary;
 using Code.Utils;
 using UnityEngine;
-using UnityEngine.InputSystem;  
+using UnityEngine.InputSystem;
 
 
 public class PlayerController : MonoBehaviour
 {
-
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private InputAction moveInput;
     [SerializeField] private InputAction dropInput;
-    
+
     [SerializeField] private SpriteRenderer inHandsRenderer;
     [SerializeField] private PickupItem currentPickupInHands;
     [SerializeField] private AudioSource sourceStepSound, sourceOrganSound, sourcePutOrganSound;
-        
+
     Rigidbody2D rb;
 
     private void Awake()
@@ -23,10 +22,11 @@ public class PlayerController : MonoBehaviour
         if (inHandsRenderer == null)
         {
             throw new NullReferenceException("inHandsIcon spriter is missing");
-        } 
+        }
+
         rb = GetComponent<Rigidbody2D>();
     }
-    
+
     void OnEnable()
     {
         moveInput.Enable();
@@ -43,7 +43,7 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 moveDirection = moveInput.ReadValue<Vector2>();
         rb.linearVelocity = moveDirection.normalized * moveSpeed;
-        if (moveDirection!=Vector2.zero && sourceStepSound != null && !sourceStepSound.isPlaying)
+        if (moveDirection != Vector2.zero && sourceStepSound != null && !sourceStepSound.isPlaying)
         {
             sourceStepSound.Play();
         }
@@ -59,7 +59,6 @@ public class PlayerController : MonoBehaviour
         {
             SetInHands(null);
         }
-        
     }
 
     public bool ReceiveOrgan(PickupItem pickup)
@@ -68,6 +67,7 @@ public class PlayerController : MonoBehaviour
         {
             return false;
         }
+
         if (pickup.pickupType == PickupType.Organ)
         {
             sourceOrganSound?.Play();
@@ -86,7 +86,7 @@ public class PlayerController : MonoBehaviour
             currentPickupInHands = null;
             return;
         }
-        
+
         inHandsRenderer.gameObject.SetActive(true);
         currentPickupInHands = pickup;
         SpriteUtils.SetPicture(pickup.icon, inHandsRenderer);
@@ -98,13 +98,18 @@ public class PlayerController : MonoBehaviour
         {
             return false;
         }
+
         return this.currentPickupInHands.name.Equals(pickup.name);
     }
-    
+
     public bool TryTakeItemFromHands()
     {
         if (this.currentPickupInHands == null) return false;
-        sourcePutOrganSound?.Play();
+        if (sourceOrganSound != null)
+        {
+            sourcePutOrganSound.Play();
+        }
+
         SetInHands(null);
         return true;
     }
